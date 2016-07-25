bf139f45dc29e217697ff36e5bd75985c6d311b8
Roberto <rnavarro70@gmail.com>
2016-07-24 21:17:39 -0500

    applied fixes, added deployment script

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // @flow
 
 import React, { Component } from 'react';
-import logo from './logo.svg'; // webpack feature
-import './App.css'; //webpack feature
+import logo from './logo.svg'; // webpack feature, ignore flow
+import './App.css'; //webpack feature, ignore flow
 
 class App extends Component {
   render():void {
+    let svg_logo = logo.replace('/', '');
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={svg_logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
