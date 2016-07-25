af1111323e15a43c77ac1f372b1761ee9ef21d06
Roberto C Navarro <rnavarro70@gmail.com>
2016-07-24 23:39:47 -0500

    renamed the app

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Class Roster</h2>
         </div>
         <p className="App-intro">
-          <h3>Hello World from Surface 4</h3>
+          <h2>Intro 1 - ACA San Antonio</h2>
         </p>
       </div>
     );
