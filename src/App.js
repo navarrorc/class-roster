8c383d6e80bf23dabee1e46e453d4e104b246369
Roberto Navarro <navarrorc@users.noreply.github.com>
2016-07-25 10:35:08 -0500

    fixed logo, added logo text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 // import logo from './logo.svg'; // webpack feature, ignore flow
-import aca_logo from './images/logo.png';  // webpack feature, ignore flow
+import aca_logo from './images/aca_circle_logo_no_text.png';  // webpack feature, ignore flow
 import './App.css'; //webpack feature, ignore flow
 
 class App extends Component {
@@ -11,10 +11,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={aca_logo} className="App-logo" alt="logo" />
-          <h2>Class Roster</h2>
+          <span id="logo_text">ACA</span>
+          <h3>Class Roster</h3>
         </div>
         <p className="App-intro">
-          <h2>Intro 1 - ACA San Antonio</h2>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, mollitia aliquam nam perferendis libero eum commodi facilis quos qui cupiditate atque praesentium earum, pariatur rerum reprehenderit saepe rem aspernatur quod.
         </p>
       </div>
     );
